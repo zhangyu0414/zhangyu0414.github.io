d84bd66a7cbb4bc22b578366b5777c2c2ad036bc
ZHDF <493743571@qq.com>
2020-02-04 16:07:18 +0800

    init resume

---
+++ src/utils/resume.js
@@ -0,0 +1,259 @@
+export default {
+    'basics': {
+        'position': 'Web 前端工程师',
+        'name': '张禹🐙',
+        'gender': '男',
+        'age': '26',
+        'worklife': '3',
+        'label': '以梦为马，不负韶华',
+        'picture': 'http://www.lgstatic.com/i/image/M00/A1/9D/CgqKkVioBICAXXVyAABFXV8M2hw21.jpeg',
+        'email': '524223270@qq.com',
+        'phone': '17306006405',
+        'website': 'https://github.com/zhangyu0414',
+        'summary': [
+            '个人主攻 Web 前端和 NodeJS 开发，3年前端开发经验',
+            '个人倾向站点（pc webapp） + 移动端开发（weapp uniapp flutter）',
+            '近期主攻hybridapp，主要使用两种技术jsbriadge做钱包app的混合开发',
+            'egg + mysql + Sequelize + vue + mpvue 实现一套小米商城（pc + admin + weapp），尚未对性能做出深度优化',
+            '喜欢关注 Web 前端相关新技术（React、Angular、Vue...），对于新技术有很强的学习兴趣',
+            '喜欢开发小程序，专注于小程序开发与研究',
+            '喜欢折腾造轮子，不定时更新 Github 小项目（https://github.com/zhangyu0414）',
+            '使用hapi、joi、Sequelize-cli构建可维护的后端架构，JWT实现身份验证设计，主要实现微信登录跟微信支付接口',
+            '热爱前端这份工作，也热爱一切美好的事物，对于我来说，是一种责任、是一种快乐',
+            '自己在后端一些深层知识还是比较薄弱，有待提高'
+            // '近期了解了 egg-vue-webpack-boilerplate 基于 Egg + Vue + Webpack 多页面和单页面服务端渲染同构工程骨架项目，以及熟悉egg webpack react方面的基础知识',
+            // '阅读 使用Egg.js+Vue.js+Docker构建渐进式、可持续集成与交付应用 这本书了解 egg在项目中构建后端 RESTful API 服务、了解docker一些运用',
+        ],
+        'location': '厦门市思明区',
+        'profiles': [
+            {
+                'network': 'github',
+                'username': 'zhangyu0414',
+                'url': 'https://github.com/zhangyu0414'
+            },
+            // {
+            //     'network': '掘金',
+            //     'username': 'zhangyu0414',
+            //     'url': 'https://www.zhihu.com/people/zhangyu0414'
+            // }
+        ]
+    },
+    'work': [
+        {
+            'company': '智合东方法务咨询有限公司',
+            'position': '2-3人 team leader',
+            'website': '',
+            'startDate': '2019.07',
+            'endDate': '至今',
+            'summary': '（ps：创业小团队）',
+            'highlights': [
+                '负责前端基础架构的搭建，自动化部署方案实施，团队代码管理，codereview，代码规范，项目进度跟踪',
+                '负责法友admin项目需求支持 vue搭建',
+                '负责法友官网需求支持，使用nuxt',
+                '负责法友app部分混合开发实现内嵌h5页面',
+                '负责公司外包商城app端实现，支持商城admin部分技术方案',
+                '负责调研app内嵌h5性能优化，已经包体积，打包速度',
+                '负责公司法友app 通过uniapp实现，为公司后面技术栈做相关调研'
+            ]
+        },
+        {
+            'company': '稿定科技',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018.09',
+            'endDate': '2019.07',
+            'summary': '稿定科技',
+            'highlights': [
+                '稿定设计主站维护',
+                '稿定PC组件库维护（主要负责业务相关组件）',
+                '移动端: m站点,微信小程序',
+                '负责小程序性能优化、主站搜索前端部分性能优化（主要做请求缓存，数据堆栈，预加载，优化请求队列 => 偏为数据层面优化）'
+            ]
+        },
+        {
+            'company': '美图公司',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2017.01',
+            'endDate': '2018.09',
+            'summary': '美图公司',
+            'highlights': [
+                '贝客钱包官网、地址浏览器',
+                '贝客钱包混合开发',
+                '贝客钱包外部分享页面',
+                '负责公司h5框架维护开发，h5服务端渲染框架维护开发',
+                '负责美图云平台前端需求迭代',
+                '负责前端技术的完善，以用户为中心，不断尝试前端新技术，提升团队水平'
+            ]
+        }
+    ],
+    'project': [
+        {
+            'organization': '图片编辑器 vue-post-editor @gaoding/editor-ui',
+            'position': '图片编辑器打杂',
+            'website': '',
+            'startDate': '2018.10',
+            'endDate': '2018.12',
+            'summary': 'vue',
+            'highlights': [
+                '参与维护 vue-post-editor @gaoding/editor-ui',
+                'https://www.npmjs.com/package/@gaoding/editor-ui',
+                'https://www.npmjs.com/package/vue-poster-editor'
+            ]
+        },
+        {
+            'organization': '贝客钱包',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018.02',
+            'endDate': '2018.09',
+            'summary': 'app',
+            'highlights': [
+                '一款去中心化钱包（区块链钱包）',
+                '活动页面：邀请好友、转盘、抽奖、咨询详情、打卡活动、任务系统'
+                // '由于安全问题还未上线，敬请期待',
+                // '由其他项目经验咱们可以详聊咯！！',
+            ]
+        },
+        {
+            'organization': '德威国际商城',
+            'position': 'Web 前端工程师',
+            'website': '',
+            'startDate': '2018.02',
+            'endDate': '2018.05',
+            'summary': '微信小程序',
+            'highlights': [
+                '商城小程序~~~',
+                'thinkjs + sql + vueadmin + 小程序（原生版本，未使用框架）',
+                // '由其他项目经验咱们可以详聊咯！！',
+            ]
+        },
+    ],
+    'education': [
+        {
+            'institution': '厦门理工学院',
+            'area': '数学与应用数学',
+            'studyType': '本科',
+            'startDate': '2013-09',
+            'endDate': '2017-06',
+            'gpa': '4.0年',
+            'courses': [
+                '数据分析',
+                '经典数学',
+                '统计学',
+                '经济学',
+                '操作系统',
+                '数据结构',
+                '编译原理'
+            ]
+        }
+    ],
+    'awards': [
+        {
+            'title': '全国计算机二级证书',
+            'date': '2015-11-01',
+            'awarder': '教育部考试中心',
+            'summary': ''
+        }
+    ],
+    'publications': [
+        {
+            'name': 'hapi',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-12',
+            'website': 'https://github.com/zhangyu0414/hapi-tutorial',
+            'summary': 'Hapi - 基于Hapi实现的小程序后端架构，通过掘金小册对hapi的一些了解，基于nodejs如何开发服务端'
+        },
+        {
+            'name': 'mpvue-ui',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-03',
+            'website': 'https://github.com/zhangyu0414/mpvue-ui',
+            'summary': 'Mpvue WeUI - 基于WeUI封装一些常用指令及服务 - 小程序ui框架'
+        },
+        {
+            'name': 'vue-blog',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-03',
+            'website': 'https://github.com/zhangyu0414/vue-blog',
+            'summary': 'vue ssr而做的博客项目，使用的是nuxt'
+        },
+        {
+            'name': 'vue-blog-admin',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-03',
+            'website': 'https://github.com/zhangyu0414/vue-blog-admin',
+            'summary': '基于vue-cli的后台管理项目。ajax请求用的axios库， ui使用的element-ui'
+        },
+        {
+            'name': 'blog-api',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-03',
+            'website': 'https://github.com/zhangyu0414/blog-api',
+            'summary': '该项目为个人博客的api接口部分，包含后端接口部分和接口文档部分。后端接口使用的是express + mongolass，接口文档使用的是swagger-ui'
+        },
+        {
+            'name': 'zhangyu0414.github.io',
+            'publisher': 'zhangyu',
+            'releaseDate': '2018-05',
+            'website': 'https://github.com/zhangyu0414/zhangyu0414.github.io',
+            'summary': 'My resume - 基于 React + React-router + ES6 前端项目'
+        },
+        {
+            'name': 'start-unit-test-demo',
+            'publisher': 'zhangyu',
+            'releaseDate': '2019-01',
+            'website': 'https://github.com/zhangyu0414/start-unit-test-demo',
+            'summary': '前端项目的单元测试，Mocha 和 Chai 的基本使用方式、Karma 是什么，如何配置 Karma、Travis-CI 是什么，怎么工作的，如何配置 Travis-CI、如何获得测试覆盖率，Codecov 是什么，大致的工作原理、大概了解 Travis-CI 和 Codecov 是怎么和 Github 配合工作的。'
+        },
+    ],
+    'skills': [
+        {
+            'name': '前端',
+            'level': 'master',
+            'keywords': [
+                'HTML',
+                'CSS',
+                'Javascript',
+                'Sass',
+                'PostCSS',
+                'Gulp',
+                'Webpack',
+                'Angular',
+                'React',
+                '小程序'
+            ]
+        },
+        {
+            'name': '后端',
+            'level': 'master',
+            'keywords': [
+                'NodeJS',
+                'Python'
+            ]
+        }
+    ],
+    'languages': [
+        {
+            'language': '中文',
+            'fluency': '母语'
+        }
+    ],
+    'interests': [
+        {
+            'name': '社交',
+            'keywords': [
+                '知乎',
+                '微博',
+                '微信',
+                'QQ'
+            ]
+        }
+    ],
+    'references': [
+        {
+            'name': 'C.A.R. Hoare',
+            'reference': '有两种软件设计的方式：一种是使它足够简单以致于明显没有缺陷，另一种则是使它足够复杂以致于没有明显的缺陷。'
+        }
+    ]
+}
